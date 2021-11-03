2f461371e9c3439e26f78968d3b310b0b0ed0298
skandru <kandru.suresh@gmail.com>
2021-11-03 09:32:16 -0700

    hands_on5

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sprimary: '#174D4D',
+        ssecondary: {
+          100:'#F8F9F9',
+          200:'#EDEDED',
+        }        
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
